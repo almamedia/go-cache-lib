d6e455af6ac1b75c2dfa3d27c4eb9cbce68723df
Linden Jouni <jouni.linden@gmail.com>
2017-10-30 09:53:31 +0200

    first commit

---
+++ cache_test.go
@@ -0,0 +1,27 @@
+package hackdaycache
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/almamedia/go-cache-lib/client"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetItem(t *testing.T) {
+	url := "https://httpbin.org/ip"
+	i := CacheItem{
+		Key:          url,
+		Value:        nil,
+		Expire:       time.Now(),
+		UpdateLength: 1 * time.Second,
+		GetFunc:      client.DataFetch,
+	}
+	AddItem(i)
+	time.Sleep(2 * time.Second)
+	assert.True(t, strings.Contains(string(GetItem(url)), "origin"))
+	cache.Remove(url)
+	assert.False(t, strings.Contains(string(GetItem(url)), "origin"))
+	assert.True(t, string(GetItem(url)) == "")
+}
---
+++ client/main/loadtest.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"math/rand"
+
+	_ "net/http/pprof"
+
+	hc "github.com/almamedia/go-cache-lib"
+	"github.com/almamedia/go-cache-lib/client"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+	for _, u := range urls {
+		item := hc.CacheItem{
+			Key:          u,
+			Value:        client.DataFetch(u),
+			Expire:       time.Now().Add(time.Duration(r1.Intn(35)+5) * time.Second),
+			UpdateLength: time.Duration(r1.Intn(35)+5) * time.Second,
+			GetFunc:      client.DataFetch,
+		}
+		hc.AddItem(item)
+	}
+	router := httprouter.New()
+	router.RedirectFixedPath = true
+	router.RedirectTrailingSlash = true
+	for _, u := range urls {
+		router.GET("/"+strings.Replace(strings.Replace(u, "http://", "", -1), "https://", "", -1), func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+			item := hc.GetItem("http:/" + r.URL.Path)
+			if item != nil {
+				w.Header().Add("Content-Type", "text/html")
+				w.WriteHeader(200)
+				w.Write(item)
+			} else {
+				w.WriteHeader(404)
+				w.Write([]byte("Not found"))
+			}
+		})
+	}
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+	go runURLs()
+	http.ListenAndServe(":8800", router)
+}
+
+func runURLs() {
+	for _, u := range urls {
+		go fetch("http://localhost:8800/"+strings.Replace(strings.Replace(u, "http://", "", -1), "https://", "", -1), "title")
+	}
+}
+
+func fetch(url, match string) {
+	t := time.Now()
+	const howMany = 5000
+	for i := 0; i < howMany; i++ {
+		item := client.DataFetch(url)
+		if item == nil || !strings.Contains(string(item), match) {
+			os.Exit(2)
+		}
+		time.Sleep(20 * time.Millisecond)
+	}
+	log.Println("fetched", howMany, "times from cache successfully in", time.Now().Sub(t))
+}
+
+var urls = []string{
+	"http://m.kauppalehti.fi/",
+	"http://www.iltalehti.fi/",
+	"http://www.aamulehti.fi/",
+	"http://www.hs.fi/",
+	"http://www.talouselama.fi/",
+	"http://www.arvopaperi.fi/",
+	"http://www.tivi.fi/",
+	"http://www.mikrobitti.fi/",
+	"http://www.tekniikkatalous.fi/",
+	"http://www.marmai.fi/",
+	"http://www.dagensmedia.se/",
+	"http://www.affarsvarlden.se/",
+	"http://www.nyteknik.se/",
+	"http://www.mtv.fi/",
+	"http://www.reuters.com/",
+	"http://www.cnbc.com/economy/",
+	"http://www.bbc.com/news/business/economy",
+	"http://www.wsj.com/news/economy",
+	"http://www.theguardian.com/business/economics",
+	"http://www.marketwatch.com/",
+	"http://money.cnn.com/",
+}
