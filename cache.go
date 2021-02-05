04860197465bc6f76ec1aab9f219cae48c98221e
Markus Heikkilä <markus.heikkila@almamedia.fi>
2021-02-05 13:56:55 +0200

    fix: items are now revoked properly.
    chore: cleaned up CacheItem, times and updateflag should not be visible outside

--- cache.go
+++ cache.go
@@ -21,7 +21,7 @@ var cacheSize = 20
 // default ttl
 var ttl = 1 * time.Hour
 
-var jobs chan CacheItem
+var jobs chan timedCacheItem
 
 // Start background loading cache with specified parameters
 func StartWith(workers, bufferSize, cacheSizeAmount int, defaultTTL time.Duration) {
@@ -35,23 +35,21 @@ func StartWith(workers, bufferSize, cacheSizeAmount int, defaultTTL time.Duratio
 // Start background loading cache with default parameters
 func Start() {
 	cache = cmap.New()
-	jobs = make(chan CacheItem, bufferedJobs)
+	jobs = make(chan timedCacheItem, bufferedJobs)
 	// workers
 	for w := 1; w <= workerAmount; w++ {
 		go worker(w, jobs)
 	}
-	go doEvery(1*time.Second, refreshAndRevoke)
+	go doEvery(1*time.Second, refresh)
+	go doEvery(1*time.Second, revoke)
 }
 
-// check and update expiring items, revoke those exceeding their TTL
-func refreshAndRevoke() {
+// check and update expiring items
+func refresh() {
 	now := time.Now()
 	for _, value := range cache.Items() {
-		item := value.(CacheItem)
-		if now.After(item.RevokeTime) && !item.Updating {
-			log.Printf("Revoking item that has not been used in %v: %v", item.TTL, item.Key)
-			cache.Remove(item.Key)
-		} else if now.After(item.ExpireTime.Add(-300*time.Millisecond)) && !item.Updating {
+		item := value.(timedCacheItem)
+		if now.After(item.ExpireTime.Add(-300*time.Millisecond)) && !item.Updating {
 			item.Updating = true
 			cache.Set(item.Key, item)
 			jobs <- item
@@ -59,83 +57,94 @@ func refreshAndRevoke() {
 	}
 }
 
-// listen to jobs channel and handle incoming items
-func worker(id int, jobs <-chan CacheItem) {
-	for item := range jobs {
-		value := item.GetFunc(item.Key)
-		if value != nil {
-			d := CacheItem{
-				Key:        item.Key,
-				Value:      value,
-				TTL: 		item.TTL,
-				RevokeTime: item.RevokeTime,
-				ExpireTime: time.Now().Add(item.Expiration),
-				Expiration: item.Expiration,
-				GetFunc:    item.GetFunc,
-				Updating:   false,
-			}
-			AddItem(d)
-		} else {
-			item.Updating = false
-			cache.Set(item.Key, item)
+// revoke those exceeding their TTL
+func revoke() {
+	now := time.Now()
+	for _, value := range cache.Items() {
+		item := value.(timedCacheItem)
+		if now.After(item.RevokeTime) && !item.Updating {
+			log.Printf("Revoking item that has not been used in %v: %v", item.TTL, item.Key)
+			cache.Remove(item.Key)
 		}
 	}
 }
 
-// GetItem value from cache
-func GetItem(key string) []byte {
+// listen to jobs channel and handle incoming items
+func worker(id int, jobs <-chan timedCacheItem) {
+	for item := range jobs {
+		value := item.GetFunc(item.Key)
+		if value != nil {
+			item.Value = value
+			item.UpdateExpireTime()
+		}
+		item.Updating = false
+		cache.Set(item.Key, item)
+	}
+}
+
+// GetValue value from cache
+func GetValue(key string) []byte {
 	value, ok := cache.Get(key)
 	if ok {
-		item := value.(CacheItem)
-		item.updateRevokeTime()
+		item := value.(timedCacheItem)
+		item.UpdateRevokeTime()
 		cache.Set(item.Key, item)
-		return value.(CacheItem).Value
+		return value.(timedCacheItem).Value
 	}
 	return nil
 }
 
-// AddItem sets the item to cache
+// AddItem sets the item to cache and updates its revoke and expire times
 func AddItem(item CacheItem) {
-	item.updateRevokeTime()
+	i := timedCacheItem{CacheItem: item}
+	i.UpdateRevokeTime()
+	i.UpdateExpireTime()
 	if cache.Count() >= cacheSize {
 		log.Print("Cache full")
 		revokeLeastViable()
 	}
-	cache.Set(item.Key, item)
+	cache.Set(i.Key, i)
 }
 
 // CacheItem for cached items
 // Key cache key, for example url
 // Value to be cached
-// Expire time to expire item
-// UpdateLength duration for next expiration
-// Get function for updating the value
+// Expiration Time to expire item. Item is refreshed using GetFunc after it expires
+// TTL Time to revocation from cache after last access
+// GetFunc function for updating the value
 type CacheItem struct {
 	Key        string
 	Value      []byte
-	ExpireTime time.Time
 	Expiration time.Duration
 	TTL        time.Duration
-	RevokeTime time.Time
 	GetFunc    func(key string) []byte
-	Updating   bool
 }
 
-func (i *CacheItem) updateRevokeTime() {
-	now := time.Now()
+type timedCacheItem struct {
+	CacheItem
+	RevokeTime time.Time
+	ExpireTime time.Time
+	Updating bool
+}
 
-	if i.TTL != 0 {
-		i.RevokeTime = now.Add(max(i.TTL, i.Expiration))
-	} else {
-		i.RevokeTime = now.Add(max(ttl, i.Expiration))
+func (i *timedCacheItem) UpdateRevokeTime() {
+	now := time.Now()
+	if i.TTL == 0 {
+		i.TTL = ttl
 	}
+	i.RevokeTime = now.Add(max(i.TTL, i.Expiration))
+}
+
+func (i *timedCacheItem) UpdateExpireTime() {
+	now := time.Now()
+	i.ExpireTime = now.Add(i.Expiration)
 }
 
 func revokeLeastViable() {
-	var earliest CacheItem
+	var earliest timedCacheItem
 	for _, v := range cache.Items() {
-		if (v.(CacheItem).RevokeTime.Before(earliest.RevokeTime) || earliest.RevokeTime == time.Time{}) {
-			earliest = v.(CacheItem)
+		if (v.(timedCacheItem).RevokeTime.Before(earliest.RevokeTime) || earliest.RevokeTime == time.Time{}) {
+			earliest = v.(timedCacheItem)
 		}
 	}
 	log.Printf("Removing cache item %s with earliest revoke time to make room", earliest.Key)
--- cache_test.go
+++ cache_test.go
@@ -14,14 +14,13 @@ func TestRevoke(t *testing.T) {
 	i := CacheItem{
 		Key:        key,
 		Value:      []byte("foo"),
-		ExpireTime: time.Now(),
 		Expiration: 1 * time.Second,
 		GetFunc:    noopGetFunc,
 	}
 	AddItem(i)
-	assert.True(t, string(GetItem(key)) == "foo")
-	time.Sleep(1500 * time.Millisecond)
-	assert.True(t, GetItem(key) == nil, "Item should have been revoked by now")
+	assert.True(t, string(GetValue(key)) == "foo")
+	time.Sleep(2000 * time.Millisecond)
+	assert.True(t, nil == GetValue(key), "Item should have been revoked by now")
 }
 
 func TestTTLLessThanExpiration(t *testing.T) {
@@ -30,23 +29,21 @@ func TestTTLLessThanExpiration(t *testing.T) {
 	i := CacheItem{
 		Key:        key,
 		Value:      []byte("foo"),
-		ExpireTime: time.Now(),
 		TTL:        1 * time.Second,
 		Expiration: 2 * time.Second,
 		GetFunc:    noopGetFunc,
 	}
 	AddItem(i)
 	time.Sleep(1500 * time.Millisecond)
-	assert.True(t, string(GetItem(key)) == "foo", "Item should still be in cache")
+	assert.True(t, string(GetValue(key)) == "foo", "Item should still be in cache")
 }
 
-func TestGetItemPostponesRevoke(t *testing.T) {
+func TestGetValuePostponesRevoke(t *testing.T) {
 	StartWith(1, 1, 1, 1*time.Second)
 	key := "xyzzy"
 	i := CacheItem{
 		Key:        key,
 		Value:      []byte("foo"),
-		ExpireTime: time.Now(),
 		TTL:        1 * time.Second,
 		Expiration: 2 * time.Second,
 		GetFunc:    noopGetFunc,
@@ -57,16 +54,16 @@ func TestGetItemPostponesRevoke(t *testing.T) {
 	if !ok {
 		t.Errorf("Should have got item %s from cache", key)
 	}
-	revokeAfterAdd := item.(CacheItem).RevokeTime
+	revokeAfterAdd := item.(timedCacheItem).RevokeTime
 	assert.True(t, now.Before(revokeAfterAdd))
 	time.Sleep(50 * time.Millisecond)
-	assert.True(t, string(GetItem(key)) == "foo", "Item should be in cache")
+	assert.True(t, string(GetValue(key)) == "foo", "Item should be in cache")
 	item, ok = cache.Get(key)
 	if !ok {
 		t.Errorf("Should have got item %s from cache", key)
 	}
-	revokeAfterGet := item.(CacheItem).RevokeTime
-	assert.True(t, revokeAfterAdd.Before(revokeAfterGet), "Revoke time should be postponed after GetItem: %v < %v", revokeAfterAdd, revokeAfterGet)
+	revokeAfterGet := item.(timedCacheItem).RevokeTime
+	assert.True(t, revokeAfterAdd.Before(revokeAfterGet), "Revoke time should be postponed after GetValue: %v < %v", revokeAfterAdd, revokeAfterGet)
 }
 
 func TestExpire(t *testing.T) {
@@ -76,13 +73,12 @@ func TestExpire(t *testing.T) {
 	i := CacheItem{
 		Key:        url,
 		Value:      value,
-		ExpireTime: time.Now(),
 		Expiration: 1 * time.Second,
 		GetFunc:    randomGetFunc,
 	}
 	AddItem(i)
 	time.Sleep(2 * time.Second)
-	assert.NotEqual(t, string(value), string(GetItem(url)), "Item should have new value in cache")
+	assert.NotEqual(t, string(value), string(GetValue(url)), "Item should have new value in cache")
 }
 
 func TestItemWithShortestTTLIsRevokedWhenCacheFillsUp(t *testing.T) {
@@ -90,7 +86,6 @@ func TestItemWithShortestTTLIsRevokedWhenCacheFillsUp(t *testing.T) {
 	AddItem(CacheItem{
 		Key: 		"1",
 		Value:      []byte("1"),
-		ExpireTime: time.Now(),
 		Expiration: 1 * time.Second,
 		TTL: 		2 * time.Second,
 		GetFunc:    noopGetFunc,
@@ -98,7 +93,6 @@ func TestItemWithShortestTTLIsRevokedWhenCacheFillsUp(t *testing.T) {
 	AddItem(CacheItem{
 		Key: 		"2",
 		Value:      []byte("2"),
-		ExpireTime: time.Now(),
 		Expiration: 1 * time.Second,
 		TTL: 		1 * time.Second,
 		GetFunc:    noopGetFunc,
@@ -106,15 +100,14 @@ func TestItemWithShortestTTLIsRevokedWhenCacheFillsUp(t *testing.T) {
 	AddItem(CacheItem{
 		Key: 		"3",
 		Value:      []byte("3"),
-		ExpireTime: time.Now(),
 		Expiration: 1 * time.Second,
 		TTL: 		2 * time.Second,
 		GetFunc:    noopGetFunc,
 	})
 
 	assert.Equal(t, 2, cache.Count(), "Cache should have 2 items")
-	assert.Equal(t, "1", string(GetItem("1")))
-	assert.Equal(t, "3", string(GetItem("3")))
+	assert.Equal(t, "1", string(GetValue("1")))
+	assert.Equal(t, "3", string(GetValue("3")))
 	for k, _ := range cache.Items() {
 		assert.NotEqual(t, "2", k, "Item #2 should have been revoked when cache was full")	
 	}
